adeea2dac0f3176c273f040c5cccc15320ef6e67
zhj <757566833@qq.com>
2022-07-24 22:33:45 +0800

    feat 添加nginx

--- explorer-client/src/pages/_app.tsx
+++ explorer-client/src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import * as React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material/styles';
+// import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '@/config/theme';
 import createEmotionCache from '@/config/createEmotionCache';
 import Layout from '@/layout';
 import Context from '@/context';
+import dynamic from 'next/dynamic'
+const DynamicHeader = dynamic<any>(() => import('@mui/material/styles').then(e=>e.ThemeProvider), {
+  ssr: false,
+})
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -23,7 +27,7 @@ export default function MyApp(props: MyAppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
+      <DynamicHeader theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Context>
@@ -32,7 +36,7 @@ export default function MyApp(props: MyAppProps) {
           </Layout>
         </Context>
 
-      </ThemeProvider>
+      </DynamicHeader>
     </CacheProvider>
   );
 }
