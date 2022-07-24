5e5161724c4714bbe77433f48f87eacd38046f5a
zhj <757566833@qq.com>
2022-07-24 22:59:41 +0800

    feature nginx 配置完成

--- explorer-client/next.config.js
+++ explorer-client/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      ...defaultPathMap,
+      '/address/index': { page: '/address/[address]'},
+      '/block/index': { page: '/block/[block]'},
+      '/tx/index': { page: '/tx/[tx]' },
+    }
+  },
 };
